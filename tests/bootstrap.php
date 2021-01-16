660e411475d1a44610598c540e52986514becad4
Emanuel Holzmann <emanuel-holzmann@web.de>
2021-01-16 19:40:01 +0100

    Add Tests Update Symfony to 5.2 CHange Card Design to show the start button

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,5 +7,5 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->usePutenv(true)->bootEnv(dirname(__DIR__).'/.env');
 }
