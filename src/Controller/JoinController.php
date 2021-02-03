e51300120af03fa0406854443c282a21190de6a7
Emanuel Holzmann <emanuel-holzmann@web.de>
2021-02-03 20:55:28 +0100

    save the name on the join page

--- src/Controller/JoinController.php
+++ src/Controller/JoinController.php
@@ -9,13 +9,16 @@ use App\Form\Type\JoinViewType;
 use App\Service\RoomService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class JoinController extends AbstractController
 {
     private $parameterBag;
+
     public function __construct(ParameterBagInterface $parameterBag)
     {
         $this->parameterBag = $parameterBag;
@@ -25,25 +28,30 @@ class JoinController extends AbstractController
      * @Route("/join/{slug}", name="join_index")
      * @Route("/join", name="join_index_no_slug")
      */
-    public function index($slug = null, Request $request, TranslatorInterface $translator, RoomService $roomService)
+    public function index($slug = null, Request $request, TranslatorInterface $translator, RoomService $roomService, HttpClientInterface $httpClient)
     {
         $data = array();
-        $server = $this->getDoctrine()->getRepository(Server::class)->findOneBy(['slug'=>$slug]);
+        $server = $this->getDoctrine()->getRepository(Server::class)->findOneBy(['slug' => $slug]);
         // dataStr wird mit den Daten uid und email encoded übertragen. Diese werden daraufhin als Vorgaben in das Formular eingebaut
         $dataStr = $request->get('data');
         $snack = $request->get('snack');
         $dataAll = base64_decode($dataStr);
         $data = array();
-        parse_str($dataAll, $data);
 
+
+
+        parse_str($dataAll, $data);
+        if ($request->cookies->get('name')){
+            $data['name']= $request->cookies->get('name');
+        }
         if (isset($data['email']) && isset($data['uid'])) {
             $room = $this->getDoctrine()->getRepository(Rooms::class)->findOneBy(['uid' => $data['uid']]);
             $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $data['email']]);
             //If the room ID is correct set and the room exists
-            if ($this->testRoomPermissions($room,$user)){
+            if ($this->testRoomPermissions($room, $user)) {
                 return $this->redirectToRoute('room_join', ['room' => $room->getId(), 't' => 'b']);
             }
-        }else {
+        } else {
             $snack = $translator->trans('Zugangsdaten in das Formular eingeben');
         }
 
@@ -60,22 +68,42 @@ class JoinController extends AbstractController
             $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $search['email']]);
 
             if (count($errors) == 0 && $room && $user && in_array($user, $room->getUser()->toarray())) {
-                if ($this->testRoomPermissions($room,$user)){
+                if ($this->testRoomPermissions($room, $user)) {
                     return $this->redirectToRoute('room_join', ['room' => $room->getId(), 't' => 'b']);
                 }
                 $url = $roomService->join($room, $user, 'b', $search['name']);
-                return $this->redirect($url);
+                $res =  $this->redirect($url);
+                $res->headers->setCookie(new Cookie('name',$search['name'], (new \DateTime())->modify('+365 days')));
+                return $res;
             }
             $snack = $translator->trans('Konferenz nicht gefunden. Zugangsdaten erneut eingeben');
         }
+        $image= null;
+        try {
+            $s = ['animal','nature','ocean','dessert','dog','cat','forest'];
+            $response = $httpClient->request('GET', 'https://api.pexels.com/v1/search?query='.$s[rand(0,6)].'&per_page=50', [
+                    'headers' => [
+                        'Authorization' => $this->parameterBag->get('laF_pexel_api_key'),
+                    ]
+                ]
+            );
+            $image = json_decode($response->getContent(),true)['photos'][rand(0,50)];
+
+        }catch (\Exception $e){
+
+        }
+
 
         return $this->render('join/index.html.twig', [
             'form' => $form->createView(),
             'snack' => $snack,
-            'server' => $server
+            'server' => $server,
+            'image'=>$image
         ]);
     }
-    function testRoomPermissions(?Rooms $room, ?User $user){
+
+    function testRoomPermissions(?Rooms $room, ?User $user)
+    {
         if ($room) {
             if (
                 $this->parameterBag->get('laF_onlyRegisteredParticipents') == 1 ||//only registered USers globaly set
