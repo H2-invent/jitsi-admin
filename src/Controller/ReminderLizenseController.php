f9c7a998513cf08535962520530b04fada0ad7f5
holema <holema>
2021-03-28 20:48:58 +0200

    remove dumps

--- src/Controller/ReminderLizenseController.php
+++ src/Controller/ReminderLizenseController.php
@@ -5,8 +5,11 @@ namespace App\Controller;
 use App\Entity\License;
 use App\Entity\Server;
 use App\Service\MailerService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
@@ -17,8 +20,13 @@ class ReminderLizenseController extends AbstractController
     /**
      * @Route("/reminder/lizense", name="reminder_lizense")
      */
-    public function index(MailerService $mailerService, TranslatorInterface $translator): Response
+    public function index(LoggerInterface $logger, Request $request, MailerService $mailerService, TranslatorInterface $translator,ParameterBagInterface $parameterBag): Response
     {
+        if ($request->get('token') !== $parameterBag->get('cronToken')) {
+            $message = ['error' => true, 'hinweis' => 'Token fehlerhaft', 'token' => $request->get('token'), 'ip' => $request->getClientIp()];
+            $logger->error($message['hinweis'], $message);
+            return new JsonResponse($message);
+        }
         $counter = 0;
         $back = (new \DateTime())->modify('+5 days');
         $now = new \DateTime();
@@ -27,25 +35,29 @@ class ReminderLizenseController extends AbstractController
             ->setParameter('now', $now)
             ->andWhere($qb->expr()->lte('license.validUntil', ':back'))
             ->setParameter('back', $back);
-
-
         $license = $qb->getQuery()->getResult();
-        dump($license);
         $em = $this->getDoctrine()->getManager();
-        foreach ($license as $data) {
-            $server = $this->getDoctrine()->getRepository(Server::class)->findOneBy(array('licenseKey' => $data->getLicenseKey()));
-           if($server){
-               dump($server);
-               $mailerService->sendEmail(
-                   $server->getAdministrator()->getEmail(),
-                   $translator->trans('Ihre Jitsi-Admin-Enterprise Lizenz läuft bald aus'),
-                   $this->renderView('email/licenseReminder.html.twig', array('server' => $server, 'license' => $data)),
-                   $server);
-           }
+        $error = false;
+        $message = '';
+        try {
+            foreach ($license as $data) {
+                $server = $this->getDoctrine()->getRepository(Server::class)->findOneBy(array('licenseKey' => $data->getLicenseKey()));
+                if($server){
+                    $mailerService->sendEmail(
+                        $server->getAdministrator()->getEmail(),
+                        $translator->trans('Ihre Jitsi-Admin-Enterprise Lizenz läuft bald aus'),
+                        $this->renderView('email/licenseReminder.html.twig', array('server' => $server, 'license' => $data)),
+                        $server);
+                    $counter++;
+                }
 
 
+            }
+        }catch (\Exception $e){
+            $error = true;
+            $message = $e->getMessage();
         }
-        return 0;
-        return new JsonResponse(array('error' => false, 'amount' => $counter));
+
+        return new JsonResponse(array('error' => $error,'message'=>$message, 'amount' => $counter));
     }
 }
