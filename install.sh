ba85d9ae67abc2e86c47f7d194521684c8eaa55c
holema <holema>
2021-04-24 15:44:01 +0200

    installer is now interactive

--- install.sh
+++ install.sh
@@ -14,9 +14,12 @@ databasePort=${databasePort:-3306}
 read -p "Enter the database name:" databaseName
 read -p "Enter the database username:" databaseUsername
 read -p"Enter the database password:" databasePassword
-replace="mysql://"$databaseUsername:$databasePassword@$databaseHost:$databasePort/$databaseName
-search="mysql://<user>:<password>@<server>:3306/<database>"
-sed -i 's/$search/$replace/' .env.local
+
+sed -i 's/<user>/$databaseUsername/' .env.local
+sed -i 's/<password>/$databasePassword/' .env.local
+sed -i 's/<server>/$databaseHost/' .env.local
+sed -i 's/<databasePort>/$databasePort/' .env.local
+sed -i 's/<database>/$databaseName/' .env.local
 
 php bin/console doctrine:schema:create
 php bin/console cache:clear
@@ -29,28 +32,28 @@ echo -------------------------------------------------------------
 echo -----------------Mailer--------------------------------------
 echo -------------------------------------------------------------
 read -p "Enter smtp host:" smtpHost
-sed -i 's/--smtpHost--/$smtpHost/' .env.local
+sed -i 's/<smtpHost>/$smtpHost/' .env.local
 read -p"Enter smtp port:" smtpPort
-sed -i 's/--smtpPort--/$smtpPort/' .env.local
+sed -i 's/<smtpPort>/$smtpPort/' .env.local
 read -p "Enter smtp username:" smtpUsername
-sed -i 's/--smtpUsername--/$smtpUsername/' .env.local
+sed -i 's/<smtpUsername>/$smtpUsername/' .env.local
 read -p "Enter smtp password:" smtpPassword
-sed -i 's/--smtpPassword--/$smtpPassword/' .env.local
+sed -i 's/<smtpPassword>/$smtpPassword/' .env.local
 read -p "Enter SMTP encrytion tls/ssln/none:" smtpEncryption
-sed -i 's/--smtpEncryption--/$smtpEncryption/' .env.local
+sed -i 's/<smtpEncryption>/$smtpEncryption/' .env.local
 echo -------------------------------------------------------------
 echo -----------------Keycloak--------------------------------------
 echo -------------------------------------------------------------
 read -p "Enter the base url of the Jits-admin:" baseUrl
-sed -i 's/--baseUrl--/$baseUrl/' .env.local
+sed -i 's/<baseUrl>/$baseUrl/' .env.local
 read -p "Enter the server of the keycloak with /auth at the and" keycloakServer
-sed -i 's/--keycloakServer--/$keycloakServer/' .env.local
+sed -i 's/<keycloakServer>/$keycloakServer/' .env.local
 read -p "Keycloak realm:" keycloakRealm
-sed -i 's/--keycloakRealm--/$keycloakRealm/' .env.local
+sed -i 's/<keycloakRealm>/$keycloakRealm/' .env.local
 read -p "Keycloak Client Id" keycloakClientId
-sed -i 's/--keycloakClientId--/$keycloakClientId/' .env.local
+sed -i 's/<keycloakClientId>/$keycloakClientId/' .env.local
 read -p "Keycloak Client Secret" keycloakClientSecret
-sed -i 's/--keycloakClientSecret--/$keycloakClientSecret/' .env.local
+sed -i 's/<keycloakClientSecret>/$keycloakClientSecret/' .env.local
 echo --------------------------------------------------------------------------
 echo -----------------They are many more parameter explore them by yourself----
 echo --------------------------------------------------------------------------
