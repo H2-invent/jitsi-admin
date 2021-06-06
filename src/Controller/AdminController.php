4ce95141281f7deb131d253ca437e5e0f3340040
holema <holema>
2021-06-06 10:23:45 +0200

    external resources can be switched off

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,6 +8,7 @@ use App\Service\AdminService;
 use Doctrine\DBAL\Types\DateType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -21,7 +22,7 @@ class AdminController extends AbstractController
      * @Route("/admin/server/{server}", name="admin_server")
      * @ParamConverter("server", class="App\Entity\Server",options={"mapping": {"server": "id"}})
      */
-    public function server(Server $server, AdminService $adminService, HttpClientInterface $httpClient, TranslatorInterface $translator)
+    public function server(ParameterBagInterface $parameterBag, Server $server, AdminService $adminService, HttpClientInterface $httpClient, TranslatorInterface $translator)
     {
         $countPart = 0;
         foreach ($server->getRooms() as $room) {
@@ -31,9 +32,11 @@ class AdminController extends AbstractController
         if ($this->getUser() !== $server->getAdministrator()) {
              return $this->redirectToRoute('dashboard',['snack'=>$translator->trans('Fehler, Der Server wurde nicht gefunden'),'color'=>'danger']);
         }
-
-        $req = $httpClient->request('GET', 'https://api.github.com/repos/H2-invent/jitsi-admin/tags');
-        $tags = json_decode($req->getContent(), true);
+        $tags = null;
+        if(!$parameterBag->get('enterprise_noExternal')){
+            $req = $httpClient->request('GET', 'https://api.github.com/repos/H2-invent/jitsi-admin/tags');
+            $tags = json_decode($req->getContent(), true);
+        }
         $chart = $adminService->createChart($server);
 
         return $this->render('admin/modalChart.html.twig', [
