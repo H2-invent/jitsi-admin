fe12d2dffdb86b675f62e424d4395369b8424c93
h2entwicklung <entwicklung@h2-invent.com>
2021-08-19 11:04:21 +0200

    when an error occured then the dashboard is reloaded

--- assets/js/refreshDashboard.js
+++ assets/js/refreshDashboard.js
@@ -10,19 +10,22 @@ function refreshDashboard() {
     var $id1 = '#ex1-tabs-1';
     var $id2 = '#ex1-tabs-2';
 
-    $div1.load(refreshUrl, function (data) {
+    $div1.load(refreshUrl, function (data, statusTxt) {
+        if(statusTxt === "error"){
+            window.location.reload();
+        }
         var $openDropdown = $('.dropdown-menu.show');
         if ($openDropdown.length === 0) {
             if ($($id1).contents().length !== $(data).find($id1).contents().length){
                 $($id1).html($(data).find($id1).contents());
-                console.log('1.2');
             }
             if ($($id2).contents().length !== $(data).find($id2).contents().length){
                 $($id2).html($(data).find($id2).contents());
-                console.log('1.3');
             }
             $('[data-toggle="popover"]').popover({html: true});
         }
+
+
     });
 }
 export {initRefreshDashboard};
