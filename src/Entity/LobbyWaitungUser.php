0bf0230c8c1ca3fd86d26f6ff2186f44a4fd5450
h2entwicklung <entwicklung@h2-invent.com>
2021-11-09 10:46:35 +0100

    lobby with camera is working now

---
+++ src/Entity/LobbyWaitungUser.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LobbyWaitungUserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LobbyWaitungUserRepository::class)
+ * @ORM\HasLifecycleCallbacks
+ */
+class LobbyWaitungUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="lobbyWaitungUsers")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Rooms::class, inversedBy="lobbyWaitungUsers")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $room;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getRoom(): ?Rooms
+    {
+        return $this->room;
+    }
+
+    public function setRoom(?Rooms $room): self
+    {
+        $this->room = $room;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
