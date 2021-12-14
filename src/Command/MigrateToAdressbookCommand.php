ac0588fef326217a1d52304602e4dc3e3517d5fe
h2entwicklung <entwicklung@h2-invent.com>
2021-12-14 10:39:49 +0100

    add a command to connect all LDAP users from the app:ldap:sync command witch each other

--- src/Command/MigrateToAdressbookCommand.php
+++ src/Command/MigrateToAdressbookCommand.php
@@ -24,7 +24,7 @@ class MigrateToAdressbookCommand extends Command
     protected function configure()
     {
         $this
-            ->setDescription('Add a short description for your command');
+            ->setDescription('This command collects all rooms which are moderator and puts the participants to the adressbook. This is only used when migrating from very old version.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
