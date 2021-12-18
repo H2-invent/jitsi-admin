42ce1a623ad870121f3997c307f3f80f73e8b3e4
h2entwicklung <entwicklung@h2-invent.com>
2021-12-18 18:46:48 +0100

    cleanup unused env

--- src/Form/Type/RoomType.php
+++ src/Form/Type/RoomType.php
@@ -33,7 +33,8 @@ class RoomType extends AbstractType
     private $paramterBag;
     private $logger;
     private $theme;
-    public function __construct(ParameterBagInterface $parameterBag, LoggerInterface $logger,ThemeService $themeService)
+
+    public function __construct(ParameterBagInterface $parameterBag, LoggerInterface $logger, ThemeService $themeService)
     {
         $this->paramterBag = $parameterBag;
         $this->logger = $logger;
@@ -42,21 +43,24 @@ class RoomType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        if (sizeof($options['server']) > 1) {
+            $builder
+                ->add('server', EntityType::class, [
+                    'choice_label' => 'serverName',
+                    'class' => Server::class,
+                    'choices' => $options['server'],
+                    'label' => 'label.serverKonferenz',
+                    'translation_domain' => 'form',
+                    'multiple' => false,
+                    'required' => true,
+                    'attr' => array('class' => 'moreFeatures')
+                ]);
+        }
 
         $builder
-            ->add('server', EntityType::class, [
-                'choice_label' => 'serverName',
-                'class' => Server::class,
-                'choices' => $options['server'],
-                'label' => 'label.serverKonferenz',
-                'translation_domain' => 'form',
-                'multiple' => false,
-                'required' => true,
-                'attr' => array('class' => 'moreFeatures')
-            ])
             ->add('name', TextType::class, ['required' => false, 'label' => 'label.konferenzName', 'translation_domain' => 'form'])
             ->add('agenda', TextareaType::class, ['required' => false, 'label' => 'label.agenda', 'translation_domain' => 'form'])
-            ->add('start', DateTimeType::class, ['required' => false, 'attr' => ['class' => 'flatpickr','placeholder'=>'placeholder.chooseTime'], 'label' => 'label.start', 'translation_domain' => 'form', 'widget' => 'single_text'])
+            ->add('start', DateTimeType::class, ['required' => false, 'attr' => ['class' => 'flatpickr', 'placeholder' => 'placeholder.chooseTime'], 'label' => 'label.start', 'translation_domain' => 'form', 'widget' => 'single_text'])
             ->add('duration', ChoiceType::class, [
                 'label' => 'label.dauerKonferenz',
                 'translation_domain' => 'form',
@@ -85,40 +89,40 @@ class RoomType extends AbstractType
                 ]
             ])
             ->add('scheduleMeeting', CheckboxType::class, array('required' => false, 'label' => 'label.scheduleMeeting', 'translation_domain' => 'form'));
-            if ($this->paramterBag->get('input_settings_persistant_rooms') == 1) {
-                $this->logger->debug('Add Persistant Rooms to the Form');
-                $builder->add('persistantRoom', CheckboxType::class, array('required' => false, 'label' => 'label.persistantRoom', 'translation_domain' => 'form'));
-            };
-            if ($this->paramterBag->get('input_settings_only_registered') == 1) {
-                $this->logger->debug('Add Only Registered Users to the Form');
-                $builder->add('onlyRegisteredUsers', CheckboxType::class, array('required' => false, 'label' => 'label.nurRegistriertenutzer', 'translation_domain' => 'form'));
-            };
-            if ($this->paramterBag->get('input_settings_share_link') == 1) {
-                $this->logger->debug('Add Share Links to the Form');
-                $builder->add('public', CheckboxType::class, array('required' => false, 'label' => 'label.puplicRoom', 'translation_domain' => 'form'));
-            };
+        if ($this->paramterBag->get('input_settings_persistant_rooms') == 1) {
+            $this->logger->debug('Add Persistant Rooms to the Form');
+            $builder->add('persistantRoom', CheckboxType::class, array('required' => false, 'label' => 'label.persistantRoom', 'translation_domain' => 'form'));
+        };
+        if ($this->paramterBag->get('input_settings_only_registered') == 1) {
+            $this->logger->debug('Add Only Registered Users to the Form');
+            $builder->add('onlyRegisteredUsers', CheckboxType::class, array('required' => false, 'label' => 'label.nurRegistriertenutzer', 'translation_domain' => 'form'));
+        };
+        if ($this->paramterBag->get('input_settings_share_link') == 1) {
+            $this->logger->debug('Add Share Links to the Form');
+            $builder->add('public', CheckboxType::class, array('required' => false, 'label' => 'label.puplicRoom', 'translation_domain' => 'form'));
+        };
 
-            if ($this->paramterBag->get('input_settings_max_participants') == 1) {
-                $this->logger->debug('Add A maximal allowed number of participants to the Form');
-                $builder->add('maxParticipants', NumberType::class, array('required' => false, 'label' => 'label.maxParticipants', 'translation_domain' => 'form', 'attr' => array('placeholder' => 'placeholder.maxParticipants')));
-            };
-           if ($this->paramterBag->get('input_settings_waitinglist') == 1) {
-               $this->logger->debug('Add a waitinglist to the Form');
-               $builder->add('waitinglist', CheckboxType::class, array('required' => false, 'label' => 'label.waitinglist', 'translation_domain' => 'form'));
-           };
-          if ($this->paramterBag->get('input_settings_conference_join_page') == 1) {
-              $this->logger->debug('Add Show Room on Joinpage to the Form');
-              $builder->add('showRoomOnJoinpage', CheckboxType::class, array('required' => false, 'label' => 'label.showRoomOnJoinpage', 'translation_domain' => 'form'));
-          };
-         if ($this->paramterBag->get('input_settings_deactivate_participantsList') == 1) {
-             $this->logger->debug('Add the possibility the users must not be on the participants list  to the Form');
-             $builder->add('totalOpenRooms', CheckboxType::class, array('required' => false, 'label' => 'label.totalOpenRooms', 'translation_domain' => 'form'))
-                 ->add('totalOpenRoomsOpenTime', NumberType::class, array('required' => false, 'label' => 'label.totalOpenRoomsOpenTime', 'translation_domain' => 'form', 'attr' => array('placeholder' => 'placeholder.maxParticipants')));
-         };
-          if ($this->paramterBag->get('input_settings_dissallow_screenshare') == 1) {
-              $this->logger->debug('Add the possibility to dissallow screenshare');
-              $builder->add('dissallowScreenshareGlobal', CheckboxType::class, array('required' => false, 'label' => 'label.dissallowScreenshareGlobal', 'translation_domain' => 'form'));
-          }
+        if ($this->paramterBag->get('input_settings_max_participants') == 1) {
+            $this->logger->debug('Add A maximal allowed number of participants to the Form');
+            $builder->add('maxParticipants', NumberType::class, array('required' => false, 'label' => 'label.maxParticipants', 'translation_domain' => 'form', 'attr' => array('placeholder' => 'placeholder.maxParticipants')));
+        };
+        if ($this->paramterBag->get('input_settings_waitinglist') == 1) {
+            $this->logger->debug('Add a waitinglist to the Form');
+            $builder->add('waitinglist', CheckboxType::class, array('required' => false, 'label' => 'label.waitinglist', 'translation_domain' => 'form'));
+        };
+        if ($this->paramterBag->get('input_settings_conference_join_page') == 1) {
+            $this->logger->debug('Add Show Room on Joinpage to the Form');
+            $builder->add('showRoomOnJoinpage', CheckboxType::class, array('required' => false, 'label' => 'label.showRoomOnJoinpage', 'translation_domain' => 'form'));
+        };
+        if ($this->paramterBag->get('input_settings_deactivate_participantsList') == 1) {
+            $this->logger->debug('Add the possibility the users must not be on the participants list  to the Form');
+            $builder->add('totalOpenRooms', CheckboxType::class, array('required' => false, 'label' => 'label.totalOpenRooms', 'translation_domain' => 'form'))
+                ->add('totalOpenRoomsOpenTime', NumberType::class, array('required' => false, 'label' => 'label.totalOpenRoomsOpenTime', 'translation_domain' => 'form', 'attr' => array('placeholder' => 'placeholder.maxParticipants')));
+        };
+        if ($this->paramterBag->get('input_settings_dissallow_screenshare') == 1) {
+            $this->logger->debug('Add the possibility to dissallow screenshare');
+            $builder->add('dissallowScreenshareGlobal', CheckboxType::class, array('required' => false, 'label' => 'label.dissallowScreenshareGlobal', 'translation_domain' => 'form'));
+        }
         if ($this->paramterBag->get('allowTimeZoneSwitch') == 1) {
             $this->logger->debug('Add the possibility to select a Timezone');
             $builder->add('timeZone', TimezoneType::class, array('required' => false, 'label' => 'label.timezone', 'translation_domain' => 'form'));
@@ -127,7 +131,7 @@ class RoomType extends AbstractType
             $this->logger->debug('Add the possibility to select the lobby');
             $builder->add('lobby', CheckboxType::class, array('required' => false, 'label' => 'label.lobby', 'translation_domain' => 'form'));
         }
-          $builder->add('submit', SubmitType::class, ['attr' => array('class' => 'btn btn-outline-primary'), 'label' => 'label.speichern', 'translation_domain' => 'form']);
+        $builder->add('submit', SubmitType::class, ['attr' => array('class' => 'btn btn-outline-primary'), 'label' => 'label.speichern', 'translation_domain' => 'form']);
     }
 
     public function configureOptions(OptionsResolver $resolver)
