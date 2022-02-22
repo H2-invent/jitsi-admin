40cf0c161616f2eef45db79b523db078b141de97
h2entwicklung <entwicklung@h2-invent.com>
2022-02-22 15:24:35 +0100

    change the direction of the favorite

--- src/Repository/RoomsRepository.php
+++ src/Repository/RoomsRepository.php
@@ -186,7 +186,9 @@ class RoomsRepository extends ServiceEntityRepository
         return $qb->innerJoin('r.favoriteUsers', 'user')
             ->andWhere('user = :user')
             ->setParameter('user', $user)
-            ->orderBy('r.start', 'ASC')
+            ->addSelect('CASE WHEN r.start IS NULL THEN 1 ELSE 0 END as HIDDEN list_order_is_null')
+            ->addOrderBy('list_order_is_null','DESC') // always ASC
+            ->addOrderBy('r.start','ASC') //DESC or ASC
             ->getQuery()
             ->getResult();
     }
