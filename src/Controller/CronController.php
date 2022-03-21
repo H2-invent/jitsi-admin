e0313e27e32b65945b91ca74e90bf7e083673c36
h2entwicklung <entwicklung@h2-invent.com>
2022-03-21 15:55:34 +0100

    add cron controller for cron bundle

--- src/Controller/CronController.php
+++ src/Controller/CronController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
-
-use App\Entity\Rooms;
 use App\Service\ReminderService;
 use App\Service\UserService;
+
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CronController extends AbstractController
@@ -25,5 +28,36 @@ class CronController extends AbstractController
             return new JsonResponse($message);
         }
         return new JsonResponse($reminderService->sendReminder());
+    }
+
+    /**
+     * @Route("/cron/run", name="cron_run")
+     */
+    public function updateCronRun(Request $request, LoggerInterface $logger, KernelInterface $kernel)
+    {
+        if ($request->get('token') !== $this->getParameter('cronToken')) {
+            $message = ['error' => true, 'hinweis' => 'Token fehlerhaft', 'token' => $request->get('token'), 'ip' => $request->getClientIp()];
+            $logger->error($message['hinweis'], $message);
+            return new JsonResponse($message);
+        }
+
+        try {
+            $application = new Application($kernel);
+            $application->setAutoExit(false);
+
+            $input = new ArrayInput([
+                'command' => 'cron:run',
+                '--script-name'=>'bin/console'
+            ]);
+
+            // You can use NullOutput() if you don't need the output
+            $output = new NullOutput();
+            $application->run($input, $output);
+
+        } catch (\Exception $exception) {
+            return new JsonResponse(array('error' => true, 'message' => $exception->getMessage()));
+        }
+
+        return new JsonResponse(array('error' => false));
     }
 }
