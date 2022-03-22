cb65f2e3184ac394e84dee1b416939c6908c0f8b
h2entwicklung <entwicklung@h2-invent.com>
2022-03-22 08:41:27 +0100

    fix migrations from prod 0.71.x

--- src/Migrations/Version20220209075616.php
+++ src/Migrations/Version20220209075616.php
@@ -20,14 +20,12 @@ final class Version20220209075616 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE analytics');
         $this->addSql('ALTER TABLE fos_user ADD indexer LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE analytics (id INT AUTO_INCREMENT NOT NULL, counter INT DEFAULT NULL, last_call DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE fos_user DROP indexer');
     }
 }
