1b7974fb7f9c41e915be61701bfd88027f004447
h2entwicklung <entwicklung@h2-invent.com>
2022-03-27 18:49:37 +0200

    add first docker try

--- installDocker.sh
+++ installDocker.sh
@@ -1,7 +1,6 @@
 echo Welcome to the installer:
 
-php bin/console doctrine:schema:create
 php bin/console cache:clear
-php bin/console doctrine:schema:update --force
-php bin/console doctrine:migrations:version --add --all
+php bin/console doctrine:schema:update --force --no-interaction
+php bin/console doctrine:migrations:version --add --all --no-interaction
 php bin/console doc:mig:mig --no-interaction
