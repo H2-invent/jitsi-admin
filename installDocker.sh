93ef28f34c1dca3be8aba5cb264f0efeef59c4ce
h2entwicklung <entwicklung@h2-invent.com>
2022-04-02 18:06:37 +0200

    add room sip caller to repeater

--- installDocker.sh
+++ installDocker.sh
@@ -13,18 +13,17 @@ else
   echo "PUBLIC_URL=$PUBLIC_URL" >> $FILE
   echo "JITSI_ADMIN_PW=$JITSI_ADMIN_PW" >> FILE
   echo "KEYCLOAK_PW=$KEYCLOAK_PW" >> FILE
-
 fi
 
-sed -i "s|**********|$NEW_UUID|g" keycloak/realm-export.json
-sed -i "s|clientUrl|$1://$2|g" keycloak/realm-export.json
+sed -i "s|<clientsecret>|$NEW_UUID|g" keycloak/realm-export.json
+sed -i "s|<clientUrl>|$HTTP_METHOD://$PUBLIC_URL|g" keycloak/realm-export.json
 sed -i "s|<jitsi-admin-pw>|$JITSI_ADMIN_PW|g" docker-entrypoint-initdb.d/init-userdb.sql
 sed -i "s|<keycloak-pw>|$KEYCLOAK_PW|g" docker-entrypoint-initdb.d/init-userdb.sql
 
 
-export PUBLIC_URL=$2
+export PUBLIC_URL=PUBLIC_URL
 export OAUTH_KEYCLOAK_CLIENT_SECRET=NEW_UUID
-export HTTP_METHOD=$1
+export HTTP_METHOD=HTTP_METHOD
 docker-compose -f docker-compose.test.yml build
 docker-compose -f docker-compose.test.yml up -d
 docker exec -d app-ja bash /var/www/dockerupdate.sh
