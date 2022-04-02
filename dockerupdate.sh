c8b1ddef65d4916861cfb01b0ad0ccf90d3bd0f2
h2entwicklung <entwicklung@h2-invent.com>
2022-04-02 17:53:20 +0200

    add room sip caller to repeater

--- dockerupdate.sh
+++ dockerupdate.sh
@@ -7,7 +7,8 @@ echo ----------------Create Database-------------------------------------------
 echo ----------------Please Backup your database-------------------------------
 echo --------------------------------------------------------------------------
 php bin/console cache:clear
-php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console doctrine:sche:up --force --no-interaction
+#php bin/console doctrine:migrations:migrate --no-interaction
 echo --------------------------------------------------------------------------
 echo -----------------Clear Cache----------------------------------------------
 echo --------------------------------------------------------------------------
@@ -18,7 +19,8 @@ echo ----------------Setting Permissin-----------------------------------------
 echo --------------------------------------------------------------------------
 chown -R www-data:www-data var/cache
 chmod -R 775 var/cache
-
+chown -R www-data:www-data public/uploads
+chmod -R 775 public/uploads
 echo --------------------------------------------------------------------------
 echo -----------------------Updated the Jitsi-Admin correct------------------
 echo --------------------------------------------------------------------------
--- installDocker.sh
+++ installDocker.sh
@@ -6,15 +6,25 @@ else
   NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   HTTP_METHOD=$1
   PUBLIC_URL=$2
-  echo "NEW_UUID=$NEW_UUID\nHTTP_METHOD=$HTTP_METHOD\nPUBLIC_URL=$PUBLIC_URL" > $FILE
-fi
+  KEYCLOAK_PW=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  JITSI_ADMIN_PW=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  echo "NEW_UUID=$NEW_UUID" >> $FILE
+  echo "HTTP_METHOD=$HTTP_METHOD" >> $FILE
+  echo "PUBLIC_URL=$PUBLIC_URL" >> $FILE
+  echo "JITSI_ADMIN_PW=$JITSI_ADMIN_PW" >> FILE
+  echo "KEYCLOAK_PW=$KEYCLOAK_PW" >> FILE
 
+fi
 
 sed -i "s|**********|$NEW_UUID|g" keycloak/realm-export.json
 sed -i "s|clientUrl|$1://$2|g" keycloak/realm-export.json
+sed -i "s|<jitsi-admin-pw>|$JITSI_ADMIN_PW|g" docker-entrypoint-initdb.d/init-userdb.sql
+sed -i "s|<keycloak-pw>|$KEYCLOAK_PW|g" docker-entrypoint-initdb.d/init-userdb.sql
+
 
 export PUBLIC_URL=$2
 export OAUTH_KEYCLOAK_CLIENT_SECRET=NEW_UUID
 export HTTP_METHOD=$1
 docker-compose -f docker-compose.test.yml build
 docker-compose -f docker-compose.test.yml up -d
+docker exec -d app-ja bash /var/www/dockerupdate.sh
