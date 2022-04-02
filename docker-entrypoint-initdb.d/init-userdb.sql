a1fd5eb1811e1634b65ba571b41beaaf1e8814c5
h2entwicklung <entwicklung@h2-invent.com>
2022-04-02 19:17:41 +0200

    add room sip caller to repeater

--- docker-entrypoint-initdb.d/init-userdb.sql
+++ docker-entrypoint-initdb.d/init-userdb.sql
@@ -1,7 +1,7 @@
-CREATE USER 'jitsiadmin'@'%' IDENTIFIED BY '<keycloak-pw>';
+CREATE USER 'jitsiadmin'@'%' IDENTIFIED BY '<jitsi-admin-pw>';
 CREATE DATABASE jitsiadmin;
 GRANT ALL PRIVILEGES ON jitsiadmin.* TO 'jitsiadmin'@'%';
-CREATE USER 'keycloak'@'%' IDENTIFIED BY '<jitsi-admin-pw>';
+CREATE USER 'keycloak'@'%' IDENTIFIED BY '<keycloak-pw>';
 CREATE DATABASE keycloak;
 GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloak'@'%';
 FLUSH PRIVILEGES;
