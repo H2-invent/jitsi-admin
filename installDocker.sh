8f7b8234c35dfe2feb0eb703e82d734ce5828cc8
h2entwicklung <entwicklung@h2-invent.com>
2022-04-02 17:31:19 +0200

    add room sip caller to repeater

--- installDocker.sh
+++ installDocker.sh
@@ -1,6 +1,14 @@
 echo Welcome to the installer:
+FILE=docker.conf
+if test -f "$FILE"; then
+  source $FILE
+else
+  NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  HTTP_METHOD=$1
+  PUBLIC_URL=$2
+  echo "NEW_UUID=$NEW_UUID\nHTTP_METHOD=$HTTP_METHOD\nPUBLIC_URL=$PUBLIC_URL" > $FILE
+fi
 
-NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 sed -i "s|**********|$NEW_UUID|g" keycloak/realm-export.json
 sed -i "s|clientUrl|$1://$2|g" keycloak/realm-export.json
