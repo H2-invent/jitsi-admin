1f9413887786642df3b0831871343d2a71afa782
h2entwicklung <entwicklung@h2-invent.com>
2022-04-03 20:42:53 +0200

    add instruction

--- src/Migrations/Version20220323074215.php
+++ src/Migrations/Version20220323074215.php
@@ -21,6 +21,10 @@ final class Version20220323074215 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE server ADD cors_header TINYINT(1) DEFAULT NULL');
+        $this->addSql('INSERT INTO cron_job (name, command, schedule,description, enabled) VALUES ("sendReminder","app:cron:sendReminder","*/10 * * * *","send reminder","1")');
+        $this->addSql('INSERT INTO cron_job (name, command, schedule,description, enabled) VALUES ("indexUser","app:index:user","0 * * * *","Reindex user","1")');
+        $this->addSql('INSERT INTO cron_job (name, command, schedule,description, enabled) VALUES ("cleanLobby"," app:lobby:cleanUp  2","0 * * *","Clean up the Lobby","1")');
+        $this->addSql('INSERT INTO cron_job (name, command, schedule,description, enabled) VALUES ("cleanReports"," cron:reports:truncate 10","0 * * *","Clean up Repots","1")');
     }
 
     public function down(Schema $schema): void
