d7aa7981649de1cf1de46655bb462d4b215dee00
h2entwicklung <entwicklung@h2-invent.com>
2022-04-03 10:38:10 +0200

    add initial mig

--- dockerupdate.sh
+++ dockerupdate.sh
@@ -7,7 +7,7 @@ echo ----------------Create Database-------------------------------------------
 echo ----------------Please Backup your database-------------------------------
 echo --------------------------------------------------------------------------
 php bin/console cache:clear
-php bin/console doctrine:sche:up --force --no-interaction
+php bin/console doctrine:mig:mig --no-interaction
 #php bin/console doctrine:migrations:migrate --no-interaction
 echo --------------------------------------------------------------------------
 echo -----------------Clear Cache----------------------------------------------
