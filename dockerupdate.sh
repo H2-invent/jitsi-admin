9fba3370c09a3e211576cb05669bd353497b234c
h2entwicklung <entwicklung@h2-invent.com>
2022-04-22 16:42:06 +0200

    set project name in docker file

--- dockerupdate.sh
+++ dockerupdate.sh
@@ -1,26 +1,26 @@
 echo --------------Schutdown Apache------------------------------------------
 #service apache2 stop
 
-
+APP=/var/www/html
 echo --------------------------------------------------------------------------
 echo ----------------Create Database-------------------------------------------
 echo ----------------Please Backup your database-------------------------------
 echo --------------------------------------------------------------------------
-php bin/console cache:clear
-php bin/console doctrine:mig:mig --no-interaction
+php $APP/bin/console cache:clear
+php $APP/bin/console doctrine:mig:mig --no-interaction
 #php bin/console doctrine:migrations:migrate --no-interaction
 echo --------------------------------------------------------------------------
 echo -----------------Clear Cache----------------------------------------------
 echo --------------------------------------------------------------------------
-php bin/console cache:clear
-php bin/console cache:warmup
+php $APP/bin/console cache:clear
+php $APP/bin/console cache:warmup
 echo --------------------------------------------------------------------------
 echo ----------------Setting Permissin-----------------------------------------
 echo --------------------------------------------------------------------------
-chown -R www-data:www-data var/cache
-chmod -R 775 var/cache
-chown -R www-data:www-data public/uploads
-chmod -R 775 public/uploads
+chown -R www-data:www-data $APP/var/cache
+chmod -R 775 $APP/var/cache
+chown -R www-data:www-data $APP/public/uploads
+chmod -R 775 $APP/public/uploads
 echo --------------------------------------------------------------------------
 echo -----------------------Updated the Jitsi-Admin correct------------------
 echo --------------------------------------------------------------------------
