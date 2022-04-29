b5bceeabbd9b414cbdf85d58b6123fd5e47f8bda
h2entwicklung <entwicklung@h2-invent.com>
2022-04-29 07:59:32 +0200

    missing div

--- src/Form/Type/KeycloakGroupsToServersType.php
+++ src/Form/Type/KeycloakGroupsToServersType.php
@@ -2,17 +2,8 @@
 namespace App\Form\Type;
 
 
-use App\Entity\AuditTomAbteilung;
 use App\Entity\KeycloakGroupsToServers;
-use League\CommonMark\Inline\Element\Text;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
