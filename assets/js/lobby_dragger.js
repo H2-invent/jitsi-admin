bdc75973a183964825a0740786f97b9d5c40b05b
h2entwicklung <entwicklung@h2-invent.com>
2022-06-04 09:34:30 +0200

    dragger in lobby

--- assets/js/lobby_dragger.js
+++ assets/js/lobby_dragger.js
@@ -2,15 +2,10 @@ import ZingTouch from 'zingtouch';
 
 function initDragDragger() {
     var activeRegion = new ZingTouch.Region(document.getElementById('frame'));
-    let childElement = document.getElementById('sliderTop');
+    let childElemeynt = document.getElementById('sliderTop');
     activeRegion.bind(childElement, 'pan', function (event) {
-        console.log(event.detail.data[0]['distanceFromOrigin'] + 'PX');
-
         var rad = event.detail.data[0]['directionFromOrigin'] / 360 * 2 * Math.PI;
-        console.log(rad + 'Rad');
-
         var y = event.detail.data[0]['distanceFromOrigin'] * Math.sin(rad)
-        console.log(y)
         if (y < -20) {
             document.getElementById('sliderTop').style.top = 0;
         } else if (y > 20) {
@@ -18,8 +13,6 @@ function initDragDragger() {
             var ele = document.getElementById('sliderTop');
             ele.style.top = top;
         }
-
-
     });
 }
 
