3e46f0ea5e338da32b5828a9d2aa7f9536eadc38
h2entwicklung <entwicklung@h2-invent.com>
2022-06-04 11:41:23 +0200

    fix dragger

--- assets/js/lobby_dragger.js
+++ assets/js/lobby_dragger.js
@@ -1,7 +1,7 @@
 import ZingTouch from 'zingtouch';
 
 function initDragDragger() {
-    var activeRegion = new ZingTouch.Region(document.getElementById('frame'),null,false);
+    var activeRegion = new ZingTouch.Region(document.getElementById('sliderTop'),null,false);
     let childElement = document.getElementById('sliderTop');
     activeRegion.bind(childElement, 'pan', function (event) {
         var rad = event.detail.data[0]['directionFromOrigin'] / 360 * 2 * Math.PI;
