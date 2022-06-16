69bc7cbe336d16edfc79abd425fc1e609e06ee81
h2entwicklung <entwicklung@h2-invent.com>
2022-06-16 17:22:32 +0200

    add host to the room

--- tests/Utils/HTTPSMaker.php
+++ tests/Utils/HTTPSMaker.php
@@ -28,14 +28,12 @@ class HTTPSMaker extends KernelTestCase
     {
         $kernel = self::bootKernel();
         //todo write test with Room and Host
+        $room = new Rooms();
+        $room->setHostUrl('https://testdomain.com');
         $createHttpsUrl = self::getContainer()->get(CreateHttpsUrl::class);
         $urlGen = self::getContainer()->get(UrlGeneratorInterface::class);
-        self::assertEquals('http://localhost:8000/', $createHttpsUrl->createHttpsUrl('/'));
-        self::assertEquals('http://localhost:8000/room/dashboard', $createHttpsUrl->createHttpsUrl('/room/dashboard'));
-        self::assertEquals('https://localhost:8000/', $createHttpsUrl->generateAbsolutUrl('http://localhost:8000', '/'));
-        self::assertEquals('https://localhost:8000/room/dashboard', $createHttpsUrl->generateAbsolutUrl('http://localhost:8000', '/room/dashboard'));
-        self::assertEquals('http://localhost:8000/room/dashboard', $createHttpsUrl->createHttpsUrl($urlGen->generate('dashboard')));
-        self::assertEquals('https://localhost:8000/room/dashboard', $createHttpsUrl->generateAbsolutUrl('http://localhost:8000', $urlGen->generate('dashboard')));
-        self::assertEquals('https://localhost:8000/room/dashboard', $createHttpsUrl->generateAbsolutUrl('https://localhost:8000', $urlGen->generate('dashboard')));
+        self::assertEquals('https://testdomain.com/testme', $createHttpsUrl->createHttpsUrl('/testme', $room));
+        $room->setHostUrl('http://testdomain.com');
+        self::assertEquals('https://testdomain.com/testme', $createHttpsUrl->createHttpsUrl('/testme', $room));
     }
 }
