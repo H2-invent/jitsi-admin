1dfc8e5df580313069fec5d2e11ef4010c160766
h2entwicklung <entwicklung@h2-invent.com>
2022-06-16 17:16:20 +0200

    add host to the room

--- src/Service/CreateHttpsUrl.php
+++ src/Service/CreateHttpsUrl.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Entity\Rooms;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -9,22 +10,38 @@ class CreateHttpsUrl
 {
     private $paramterBag;
     private $request;
+
     public function __construct(RequestStack $requestStack, ParameterBagInterface $parameterBag)
     {
         $this->paramterBag = $parameterBag;
         $this->request = $requestStack;
     }
 
-    public function createHttpsUrl($url){
-        if ($this->paramterBag->get('laF_baseUrl') !=''){
-            return $this->paramterBag->get('laF_baseUrl').$url;
-        }else{
-            return $this->generateAbsolutUrl( $this->request->getCurrentRequest()->getSchemeAndHttpHost(),$url);
+    public function createHttpsUrl($url, ?Rooms $rooms = null)
+    {
+        if ($this->paramterBag->get('LAF_DEV_URL') !== '') {
+            return $this->paramterBag->get('LAF_DEV_URL') . $url;
+        } else {
+            if ($rooms && $rooms->getHostUrl()) {
+                return $this->generateAbsolutUrl($rooms->getHostUrl(), $url);
+            }
+
+            try {
+                if ($this->request && $this->request->getCurrentRequest()) {
+                    return $this->generateAbsolutUrl($this->request->getCurrentRequest()->getSchemeAndHttpHost(), $url);
+                } else {
+                    return $this->paramterBag->get('laF_baseUrl') . $url;
+                }
+            } catch (\Exception $exception) {
+                return $this->paramterBag->get('laF_baseUrl') . $url;
+            }
         }
+
     }
 
-    public function generateAbsolutUrl($baseUrl, $url){
-        return str_replace('http://','https://',$baseUrl).$url;
+    public function generateAbsolutUrl($baseUrl, $url)
+    {
+        return str_replace('http://', 'https://', $baseUrl) . $url;
     }
 
 }
--- src/Twig/HttpsAbsolutUrl.php
+++ src/Twig/HttpsAbsolutUrl.php
@@ -3,6 +3,7 @@
 namespace App\Twig;
 
 
+use App\Entity\Rooms;
 use App\Service\CreateHttpsUrl;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -15,6 +16,7 @@ class HttpsAbsolutUrl extends AbstractExtension
 
     private $httpsUrl;
     private $paramterBag;
+
     public function __construct(CreateHttpsUrl $createHttpsUrl, ParameterBagInterface $parameterBag)
     {
         $this->httpsUrl = $createHttpsUrl;
@@ -29,9 +31,9 @@ class HttpsAbsolutUrl extends AbstractExtension
         ];
     }
 
-    public function httpsAbolutUrl($url)
+    public function httpsAbolutUrl($url, ?Rooms $rooms = null)
     {
-        return $this->httpsUrl->createHttpsUrl($url);
+        return $this->httpsUrl->createHttpsUrl($url, $rooms);
 
     }
 }
