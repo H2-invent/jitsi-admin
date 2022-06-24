393d87f4cf1c7e0f9f7e18bb1f7569c64b18590e
h2entwicklung <entwicklung@h2-invent.com>
2022-06-24 07:43:07 +0200

    remove hotkey

--- assets/js/init.js
+++ assets/js/init.js
@@ -18,33 +18,33 @@ function initGenerell() {
     initLayzLoading();
     openBlankTarget(blankTarget);
     initAdhocMeeting(confirmTitle, confirmCancel, confirmOk);
-    $("body").keypress(function (e) {
-        var pressed = e.which
-        if (pressed === 49) {//1 is pressed
-            $('#ex1-tab-1-tab').trigger('click');
-        } else if (pressed === 50) {//2 is pressed
-            $('#ex1-tab-3-tab').trigger('click');
-        } else if (pressed === 51) {//3 is pressed
-            $('#ex1-tab-2-tab').trigger('click');
-        }else if(pressed ===110){
-            $('#createNewConference').trigger('click');
-        }else if(pressed ===97){
-            const myModalEl = document.getElementById('modalAdressbook');
-            if (!myModalEl.classList.contains('show')){
-                const modal = new mdb.Modal(myModalEl)
-                modal.show();
-            }
-            $('#home-tab').trigger('click');
-
-        }else if(pressed ===103){
-            const myModalEl = document.getElementById('modalAdressbook');
-            if (!myModalEl.classList.contains('show')){
-                const modal = new mdb.Modal(myModalEl)
-                modal.show();
-            }
-            $('#profile-tab').trigger('click');
-        }
-    });
+    // $("body").keypress(function (e) {
+    //     var pressed = e.which
+    //     if (pressed === 49) {//1 is pressed
+    //         $('#ex1-tab-1-tab').trigger('click');
+    //     } else if (pressed === 50) {//2 is pressed
+    //         $('#ex1-tab-3-tab').trigger('click');
+    //     } else if (pressed === 51) {//3 is pressed
+    //         $('#ex1-tab-2-tab').trigger('click');
+    //     }else if(pressed ===110){
+    //         $('#createNewConference').trigger('click');
+    //     }else if(pressed ===97){
+    //         const myModalEl = document.getElementById('modalAdressbook');
+    //         if (!myModalEl.classList.contains('show')){
+    //             const modal = new mdb.Modal(myModalEl)
+    //             modal.show();
+    //         }
+    //         $('#home-tab').trigger('click');
+    //
+    //     }else if(pressed ===103){
+    //         const myModalEl = document.getElementById('modalAdressbook');
+    //         if (!myModalEl.classList.contains('show')){
+    //             const modal = new mdb.Modal(myModalEl)
+    //             modal.show();
+    //         }
+    //         $('#profile-tab').trigger('click');
+    //     }
+    // });
 }
 
 function openBlankTarget(targets) {
