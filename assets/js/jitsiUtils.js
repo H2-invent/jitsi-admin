50214b37c97608b37f5051803bde8cee6e130389
h2entwicklung <entwicklung@h2-invent.com>
2022-06-26 11:15:46 +0200

    adjust width for drag on tablet

--- assets/js/jitsiUtils.js
+++ assets/js/jitsiUtils.js
@@ -56,19 +56,19 @@ function initJitsi(options, domain, titelL, okL, cancelL) {
                 content: hangupQuestion,
                 theme: 'material',
                 buttons: {
-                    confirm: {
-                        text: hangupText, // text for button
-                        btnClass: 'btn-outline-danger btn', // class for the button
-                        action: function () {
-                            api.executeCommand('hangup');
-                        },
-                    },
                     killAll: {
                         text: endMeetingText, // text for button
                         btnClass: 'btn-outline-danger btn', // class for the button
                         action: function () {
                             endMeeting();
                             $.getJSON(endMeetingUrl);
+                        },
+                    },
+                    confirm: {
+                        text: hangupText, // text for button
+                        btnClass: 'btn-outline-danger btn', // class for the button
+                        action: function () {
+                            api.executeCommand('hangup');
                         },
                     },
                     cancel: {
