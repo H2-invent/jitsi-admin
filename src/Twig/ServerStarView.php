320e64cf90694f88f97f0c4853091ef2c8573c8a
h2entwicklung <entwicklung@h2-invent.com>
2022-07-17 12:03:07 +0200

    show stars

---
+++ src/Twig/ServerStarView.php
@@ -0,0 +1,35 @@
+<?php
+// src/Twig/AppExtension.php
+namespace App\Twig;
+
+use App\Service\MessageService;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use function GuzzleHttp\Psr7\str;
+
+class ServerStarView extends AbstractExtension
+{
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('showAverageStar', [$this, 'showAverageStar']),
+        ];
+    }
+
+    public function showAverageStar(\App\Entity\Server $server)
+    {
+        $star = 0;
+        $count = 0;
+        foreach ($server->getStars() as $data){
+            $star += $data->getStar();
+            $count++;
+        }
+        if ($count>0){
+            $star = $star/$count;
+        }
+
+        return $star;
+    }
+}
