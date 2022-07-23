4f7b9bf2c0ce05487c81e878a1d45f119417edc7
h2entwicklung <entwicklung@h2-invent.com>
2022-07-23 14:29:32 +0200

    reformat the entity with rector. Rector is not working sample.

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\Symfony\Set\SensiolabsSetList;
+use Rector\Nette\Set\NetteSetList;
+use Rector\Config\RectorConfig;
+
+return function (RectorConfig $rectorConfig): void {
+
+    $rectorConfig->sets([
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+        NetteSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        SensiolabsSetList::FRAMEWORK_EXTRA_61,
+    ]);
+};
