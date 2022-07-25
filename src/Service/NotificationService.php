7c4b33d90a23b276baf0b0e355f234472823282e
h2entwicklung <entwicklung@h2-invent.com>
2022-07-25 14:03:04 +0200

    add agenda in ics

--- src/Service/NotificationService.php
+++ src/Service/NotificationService.php
@@ -48,10 +48,11 @@ class NotificationService
         if ($user->getTimeZone()) {
             $this->ics->setTimezoneId($user->getTimeZone());
         }
+        dump(explode("\r\n",$rooms->getAgenda()));
         $description =   $this->translator->trans('Sie wurden zu einer Videokonferenz auf dem Jitsi Server {server} hinzugefügt.', array('{server}' => $rooms->getServer()->getServerName())) .
             '\n\n' .
             $this->translator->trans('Über den beigefügten Link können Sie ganz einfach zur Videokonferenz beitreten.\nName: {name} \nModerator: {moderator} ', array('{name}' => $rooms->getName(), '{moderator}' => $rooms->getModerator()->getFirstName() . ' ' . $rooms->getModerator()->getLastName()))
-            . '\n\n' .
+            .($rooms->getAgenda()?'\n\n'.$this->translator->trans('Agenda').'\n'. implode('\n',explode("\r\n",$rooms->getAgenda())).'\n\n':'\n\n').
             $this->translator->trans('Folgende Daten benötigen Sie um der Konferenz beizutreten:\nKonferenz ID: {id} \nIhre E-Mail-Adresse: {email}', array('{id}' => $rooms->getUid(), '{email}' => $user->getEmail()))
             . '\n\n' .
             $url .
