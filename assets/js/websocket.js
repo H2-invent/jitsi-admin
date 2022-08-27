e3e02c0a77457e0711fc48dfafb9567a8815a609
h2entwicklung <entwicklung@h2-invent.com>
2022-08-27 17:19:19 +0200

    show in meeting when Iframe is opened

--- assets/js/websocket.js
+++ assets/js/websocket.js
@@ -12,51 +12,54 @@ export function initWebsocket(jwt) {
     socket = io(websocketUrl, {
         query: {token}
     });
-
-    socket.on('connect', function (data) {
-        if (!inIframe()){
-            getStatus();
-        }
-    });
-    socket.on('sendOnlineUser', function (data) {
-        if (!inIframe()){
-            showOnlineUsers(JSON.parse(data))
-        }
-    })
     socket.on('mercure', function (data) {
         masterNotify(JSON.parse(data));
     })
 
     var hidden, visibilityChange;
-    if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
-        hidden = "hidden";
-        visibilityChange = "visibilitychange";
-    } else if (typeof document.msHidden !== "undefined") {
-        hidden = "msHidden";
-        visibilityChange = "msvisibilitychange";
-    } else if (typeof document.webkitHidden !== "undefined") {
-        hidden = "webkitHidden";
-        visibilityChange = "webkitvisibilitychange";
-    }
 
-    function handleVisibilityChange() {
-        if (document[hidden]) {
-        } else {
-            if (!inIframe()){
-                sendViaWebsocket('stillOnline');
+    if (!inIframe()) {
+        socket.on('connect', function (data) {
+            if (!inIframe()) {
+                getStatus();
+            }
+        });
+        socket.on('sendOnlineUser', function (data) {
+            if (!inIframe()) {
+                showOnlineUsers(JSON.parse(data))
+            }
+        })
+        if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+            hidden = "hidden";
+            visibilityChange = "visibilitychange";
+        } else if (typeof document.msHidden !== "undefined") {
+            hidden = "msHidden";
+            visibilityChange = "msvisibilitychange";
+        } else if (typeof document.webkitHidden !== "undefined") {
+            hidden = "webkitHidden";
+            visibilityChange = "webkitvisibilitychange";
+        }
+
+        function handleVisibilityChange() {
+            if (document[hidden]) {
+            } else {
+                if (!inIframe()) {
+                    sendViaWebsocket('stillOnline');
+                }
             }
         }
+
+        if (typeof document.addEventListener === "undefined" || typeof document[hidden] === "undefined") {
+            console.log(" requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+        } else {
+            document.addEventListener(visibilityChange, handleVisibilityChange, false);
+        }
+
+        setInterval(function () {
+            handleVisibilityChange();
+        }, 60000)
     }
 
-    if (typeof document.addEventListener === "undefined" || typeof document[hidden] === "undefined") {
-        console.log(" requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
-    } else {
-        document.addEventListener(visibilityChange, handleVisibilityChange, false);
-    }
-
-    setInterval(function () {
-        handleVisibilityChange();
-    }, 60000)
 }
 
 export function sendViaWebsocket(event, message) {
