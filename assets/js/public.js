f790d3b39b04d3f1479cc96ae30136a5980c8106
h2entwicklung <entwicklung@h2-invent.com>
2022-10-19 20:04:05 +0200

    add public roome invitation

---
+++ assets/js/public.js
@@ -0,0 +1,18 @@
+import * as mdb from 'mdb-ui-kit'; // lib
+import ClipboardJS from 'clipboard'
+
+
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+docReady(function() {
+    document.getElementById('linkPart').value = window.location.href
+    var clipboard = new ClipboardJS('.copyLink');
+});
