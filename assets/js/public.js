d0965a6e739d58996ceaf418a5a9da2223a3179b
h2entwicklung <entwicklung@h2-invent.com>
2022-10-19 20:53:22 +0200

    add public roome invitation

--- assets/js/public.js
+++ assets/js/public.js
@@ -3,6 +3,15 @@ import ClipboardJS from 'clipboard'
 
 
 
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
 docReady(function() {
     var clipboard = new ClipboardJS('.copyLink');
 });
