4b10fc2c6cb34c7d37579ec6bfcc303df1222731
h2entwicklung <entwicklung@h2-invent.com>
2022-10-21 10:44:56 +0200

    integrate etherpad

--- assets/js/startWhiteboard.js
+++ assets/js/startWhiteboard.js
@@ -3,32 +3,36 @@ import {sendViaWebsocket} from "./websocket";
 import {createIframe} from "./createConference";
 
 export function initStartWhiteboard() {
-    if (!document.querySelector('.startWhiteboard')){
+    if (!document.querySelector('.startExternalApp')){
         return null;
     }
-    var ele = document.querySelector('.startWhiteboard');
-    ele.classList.remove('d-none');
-    ele.addEventListener('click', function (ev) {
-        var selfurl = this.dataset.selfurl;
-        var url = this.dataset.url;
-        if ( this.dataset.room){
-            var message = {
-                room: this.dataset.room,
-                url: url,
-                title: document.title
-            }
-            sendViaWebsocket('openNewIframe',JSON.stringify(message));
-        }
+    var eles = document.querySelectorAll('.startExternalApp');
+    for (var ele of eles) {
 
-        if (inIframe()){
-            const parentMessage = JSON.stringify({
-                type: 'openNewIframe',
-                url: selfurl,
-                'title': document.title
-            });
-            window.parent.postMessage(parentMessage, '*');
-        }else {
-            createIframe(selfurl,document.title,false);
-        }
-    })
+
+        ele.classList.remove('d-none');
+        ele.addEventListener('click', function (ev) {
+            var selfurl = this.dataset.selfurl;
+            var url = this.dataset.url;
+            if (this.dataset.room) {
+                var message = {
+                    room: this.dataset.room,
+                    url: url,
+                    title: document.title
+                }
+                sendViaWebsocket('openNewIframe', JSON.stringify(message));
+            }
+
+            if (inIframe()) {
+                const parentMessage = JSON.stringify({
+                    type: 'openNewIframe',
+                    url: selfurl,
+                    'title': document.title
+                });
+                window.parent.postMessage(parentMessage, '*');
+            } else {
+                createIframe(selfurl, document.title, false);
+            }
+        })
+    }
 }
