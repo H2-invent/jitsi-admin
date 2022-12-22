757fb135b94075b6933fece24d146b47fb4c6e6a
h2entwicklung <entwicklung@h2-invent.com>
2022-12-22 19:07:26 +0100

    toggle conference manuell

--- assets/js/moderatorIframe.js
+++ assets/js/moderatorIframe.js
@@ -1,36 +1,48 @@
 import {pauseConference, playConference} from "./jitsiUtils";
 
 global.frameId = null;
+var blockPause = null;
+
 function initModeratorIframe(closeFkt) {
 
     window.addEventListener('message', function (e) {
 
         const decoded = JSON.parse(e.data);
-        window.parent.postMessage(JSON.stringify({type:'ack',messageId:decoded.messageId}), '*');
-         if (decoded.type === 'init') {
+        window.parent.postMessage(JSON.stringify({type: 'ack', messageId: decoded.messageId}), '*');
+        if (decoded.type === 'init') {
             frameId = decoded.frameId;
-        }else if(decoded.type === 'pleaseClose'){
-             if (typeof decoded.frameId !== 'undefined'){
-                 frameId = decoded.frameId;
-             }
-             closeFkt();
-        }else if(decoded.type === 'pauseIframe'){
-             console.log('pause COnference')
-             pauseConference();
-         }
-         else if(decoded.type === 'playIframe'){
-             if (typeof decoded.frameId !== 'undefined'){
-                 console.log('play COnference')
-                 playConference();
-             }
-         }
+        } else if (decoded.type === 'pleaseClose') {
+            if (typeof decoded.frameId !== 'undefined') {
+                frameId = decoded.frameId;
+            }
+            closeFkt();
+        } else if (decoded.type === 'pauseIframe') {
+
+            if (blockPause!== true){
+                blockPause = true;
+                pauseConference().then(function () {
+                    blockPause = false
+                });
+            }
+
+        } else if (decoded.type === 'playIframe') {
+            if (typeof decoded.frameId !== 'undefined') {
+                if (blockPause !== true) {
+                    blockPause = true;
+                    playConference().then(function () {
+                        blockPause = false
+                    });
+                }
+
+            }
+        }
     });
 }
 
 function close(frameIdTmp) {
-    if (inIframe()){
-        var id = frameIdTmp?frameIdTmp:frameId
-        if (id){
+    if (inIframe()) {
+        var id = frameIdTmp ? frameIdTmp : frameId
+        if (id) {
             const message = JSON.stringify({
                 type: 'closeMe',
                 frameId: id
@@ -39,11 +51,13 @@ function close(frameIdTmp) {
         }
     }
 }
-function inIframe () {
+
+function inIframe() {
     try {
         return window.self !== window.top;
     } catch (e) {
         return true;
     }
 }
-export {initModeratorIframe,close, inIframe}
+
+export {initModeratorIframe, close, inIframe}
