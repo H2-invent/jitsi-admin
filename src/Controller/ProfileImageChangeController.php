2963a43fa979c52ab3dea3c5fcb4ff820f8c06f0
h2entwicklung <entwicklung@h2-invent.com>
2023-03-13 12:49:09 +0100

    ringing in callout

---
+++ src/Controller/ProfileImageChangeController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\Type\ProfileImageType;
+use App\Form\Type\SecondEmailType;
+use App\Form\Type\TimeZoneType;
+use App\Helper\JitsiAdminController;
+
+use http\Exception\InvalidArgumentException;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+
+class ProfileImageChangeController extends JitsiAdminController
+{
+
+
+    /**
+     * @Route("/room/profileImage/change", name="profile_image_change")
+     */
+    public function index(Request $request, TranslatorInterface $translator): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileImageType::class, $user, ['action' => $this->generateUrl('profile_image_save')]);
+        return $this->render('time_zone/index.html.twig', array(
+            'form' => $form->createView(),
+            'title' => $translator->trans('second.email.title')
+        ));
+    }
+
+    /**
+     * @Route("/room/profileImage/save", name="profile_image_save")
+     */
+    public function new(Request $request, TranslatorInterface $translator, LoggerInterface $logger, ValidatorInterface $validator): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfileImageType::class, $user, ['action' => $this->generateUrl('profile_image_save')]);
+        try {
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $user = $form->getData();
+                $error = $validator->validate($user->getProfilePicture());
+                if (sizeof($error)){
+                    foreach ($error as $data){
+                        $this->addFlash('danger', $data->getMessage());
+                    }
+
+                    return $this->redirectToRoute('dashboard');
+                }
+
+                if ($user->getSecondEmail()) {
+                    foreach (explode(',', $user->getSecondEmail()) as $data) {
+                        if (!filter_var(trim($data), FILTER_VALIDATE_EMAIL)) {
+                            throw new \InvalidArgumentException('Invalid Email: ' . $data);
+                        }
+                    }
+                }
+
+                $user->getProfilePicture()->setUpdatedAt(new \DateTime());
+                $user->setUpdatedAt(new \DateTime());
+                $em = $this->doctrine->getManager();
+                $em->persist($user);
+                $em->flush();
+                $user = $this->getUser();
+                if ($user->getProfilePicture() && !$user->getProfilePicture()->getDocumentFileName()) {
+                    $user->setProfilePicture(null);
+                    $em->persist($user);
+                    $em->flush();
+                }
+            }
+        } catch (\InvalidArgumentException $exception) {
+            $logger->error($exception->getMessage());
+            $this->addFlash('danger', $translator->trans('Ungültige Email. Bitte überprüfen Sie ihre Emailadresse.'));
+            return $this->redirectToRoute('dashboard');
+        } catch (\Exception $exception) {
+            $logger->error($exception->getMessage());
+            $this->addFlash('danger', $translator->trans('Fehler'));
+            return $this->redirectToRoute('dashboard');
+        }
+        $this->addFlash('success', $translator->trans('CC-E-Mails erfolgreich geändert auf: {secondEmails}', array('{secondEmails}' => $user->getSecondEmail())));
+        return $this->redirectToRoute('dashboard');
+    }
+}
--- src/Form/Type/SecondEmailType.php
+++ src/Form/Type/SecondEmailType.php
@@ -9,6 +9,7 @@
 namespace App\Form\Type;
 
 use App\Entity\User;
+use App\Service\ThemeService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -18,19 +19,30 @@ use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class SecondEmailType extends AbstractType
 {
+    public function __construct(private ThemeService $themeService)
+    {
+
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 
-        $builder
-            ->add('secondEmail', TextType::class, ['required' => false, 'label' => 'label.secondEmail', 'translation_domain' => 'form','help'=>'help.secondEmail'])
-            ->add('profilePicture', ImageType::class,['label' => 'label.profilImage', 'translation_domain' => 'form'])
-            ->add('submit', SubmitType::class, ['attr' => array('class' => 'btn btn-outline-primary'), 'label' => 'label.speichern', 'translation_domain' => 'form']);
+        $builder ->add('profilePicture', ImageType::class,['label' => 'label.profilImage', 'translation_domain' => 'form'])
+            ->add('submit', SubmitType::class, ['attr' => array('class' => 'btn btn-primary'), 'label' => 'label.speichern', 'translation_domain' => 'form']);
+        if ($this->themeService->getApplicationProperties('allowTimeZoneSwitch')){
+            $builder->add('timeZone', \Symfony\Component\Form\Extension\Core\Type\TimezoneType::class, ['required' => false, 'label' => 'label.timezone', 'translation_domain' => 'form']);
+        }
+
+        if (!$this->themeService->getTheme() || $this->themeService->getApplicationProperties('profileAllowSecondEmail')){
+            $builder->add('secondEmail', TextType::class, ['required' => false, 'label' => 'label.secondEmail', 'translation_domain' => 'form','help'=>'help.secondEmail']);
+        }
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-          'data_class' => User::class,
+          'data_class' => User::class
         ]);
 
     }
