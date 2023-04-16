79597a0fb5e727b290f335d681508aa24c473440
h2entwicklung <entwicklung@h2-invent.com>
2023-04-16 20:37:49 +0200

    add a nginx configuration

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt install -y lsb-release ca-certificates apt-transport-https software-pro
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
 curl -fsSL  https://packages.sury.org/php/apt.gpg| sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
 sudo apt update
-sudo apt install php8.1 nginx nginx-extras -y
+sudo apt install curl php8.1 nginx nginx-extras -y
 sudo apt install php8.1-bcmath php8.1-fpm php8.1-xml php8.1-mysql php8.1-zip php8.1-intl php8.1-ldap php8.1-gd php8.1-cli php8.1-bz2 php8.1-curl php8.1-mbstring php8.1-pgsql php8.1-opcache php8.1-soap php8.1-cgi php8.1-dom php8.1-simplexml -y
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt -y install nodejs
