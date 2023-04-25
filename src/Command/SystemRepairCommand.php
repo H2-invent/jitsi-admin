9107aa5da3491940a12773a23567468a441585bf
h2entwicklung <entwicklung@h2-invent.com>
2023-04-25 10:30:15 +0200

    * fix emails which are doubled * Write all repairs in a logfile

--- src/Command/SystemRepairCommand.php
+++ src/Command/SystemRepairCommand.php
@@ -6,6 +6,7 @@ use App\Controller\DashboardController;
 use App\Entity\LobbyWaitungUser;
 use App\Entity\Rooms;
 use App\Entity\User;
+use App\Service\ldap\LdapUserService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -18,9 +19,10 @@ class SystemRepairCommand extends Command
     protected static $defaultName = 'app:system:repair';
     protected static $defaultDescription = 'Add a short description for your command';
     private $em;
-    private  SymfonyStyle $io;
-
-    public function __construct(EntityManagerInterface $entityManager, string $name = null)
+    private SymfonyStyle $io;
+    private string $logfile = 'repairLog.txt';
+    private $logFileFile;
+    public function __construct(private LdapUserService $ldapUserService, EntityManagerInterface $entityManager, string $name = null)
     {
         parent::__construct($name);
         $this->em = $entityManager;
@@ -37,13 +39,18 @@ class SystemRepairCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $this->io = $io;
         $io->info('We try to repair the system.....');
+        $this->logFileFile = fopen($this->logfile, "a") or die("Unable to open file!");
+        fwrite($this->logFileFile,sprintf(PHP_EOL.PHP_EOL.'Repair on %s'.PHP_EOL,(new \DateTime())->format('d.m.Y H:i')));
         $count = 0;
         $user = $this->em->getRepository(User::class)->findAll();
         $io->info('--------We start with the users------');
-        foreach ($user as $u){
+        fwrite($this->logFileFile,sprintf('Repair emails with newline'.PHP_EOL));
+
+        foreach ($user as $u) {
             $this->repairEmail(user: $u);
         }
         $this->em->flush();
+        $this->findDoubleEmail();
         $rooms = $this->em->getRepository(Rooms::class)->findAll();
 
         foreach ($rooms as $room) {
@@ -67,7 +74,7 @@ class SystemRepairCommand extends Command
         $user = $this->em->getRepository(User::class)->findAll();
         $count += $this->repairWaitungUser();
         $io->success(sprintf('We found %d coruppt datasets', $count));
-
+        fclose($this->logFileFile);
         return Command::SUCCESS;
     }
 
@@ -91,10 +98,70 @@ class SystemRepairCommand extends Command
     {
         $emailOrg = $user->getEmail();
         $email = rtrim($user->getEmail());
-        if ($email!== $emailOrg){
-            $this->io->info(sprintf('-------%s was corrupt--------',$email));
+
+        if ($email !== $emailOrg) {
+            $this->io->info(sprintf('-------%s was corrupt--------', $email));
+
+            fwrite($this->logFileFile,sprintf('Email with newline found %s in user id %d'.PHP_EOL,$email,$user->getId()));
             $user->setEmail(email: $email);
             $this->em->persist($user);
         }
+    }
+
+    private function findDoubleEmail()
+    {
+        $user = $this->em->getRepository(User::class)->findAll();
+        $checked = array();
+        $count = 0;
+        $countWithAccount = 0;
+        foreach ($user as $u) {
+            $email = $u->getEmail();
+            if (!in_array($email, $checked) && $email !== 'test1@local.h2') {
+                $allEmails = $this->em->getRepository(User::class)->findBy(array('email' => $email));
+                $checked[] = $email;
+
+                if (sizeof($allEmails) > 1) {
+                    $count++;
+                    $this->io->info(sprintf('-----Double %s Email----', $email));
+                    $loggedIn = null;
+                    foreach ($allEmails as $d) {
+                        if ($d->getKeycloakId()) {
+                            $loggedIn = $d;
+                        }
+                    }
+                    if (!$loggedIn) {
+                        $loggedIn = $allEmails[0];
+                    } else {
+                        $countWithAccount++;
+                        $this->io->info(sprintf('-----Has Account %s %s Email----', $loggedIn->getFirstName(), $loggedIn->getLastName()));
+                        fwrite($this->logFileFile,sprintf('Email %s with id %d has an account and has to stay'.PHP_EOL,$loggedIn->getEmail(),$loggedIn->getId()));
+                    }
+                    foreach ($allEmails as $email) {
+                        if ($email !== $loggedIn) {
+                            fwrite($this->logFileFile,sprintf('Double email found %s in user id %d'.PHP_EOL,$email->getEmail(),$email->getId()));
+                            foreach ($email->getRooms() as $room) {
+                                $loggedIn->addRoom($room);
+                                fwrite($this->logFileFile,sprintf('Add Room  with id %d from email %s to %s with id %d'.PHP_EOL,$room->getId(),$email->getEmail(), $loggedIn->getEmail(), $loggedIn->getId()));
+                            }
+                            foreach ($email->getAddressbookInverse() as $adressbook) {
+                                $loggedIn->addAddressbookInverse($adressbook);
+
+                            }
+                            foreach ($email->getSchedulingTimeUsers() as $schedulingTimeUser) {
+                                $loggedIn->addSchedulingTimeUser($schedulingTimeUser);
+                            }
+                            fwrite($this->logFileFile,sprintf('Delete User  with id %d and email %s'.PHP_EOL,$email->getId(),$email->getEmail()));
+                            $this->ldapUserService->deleteUser($email);
+
+                        }
+                    }
+                    $this->em->persist($loggedIn);
+
+                }
+            }
+        }
+        $this->em->flush();
+        $this->io->info(sprintf('------Found %d double emails', $count));
+        $this->io->info(sprintf('------Found %d account ', $countWithAccount));
     }
 }
