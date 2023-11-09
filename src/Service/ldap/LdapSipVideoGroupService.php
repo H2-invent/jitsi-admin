7309624fbb7276bca514ce7f08876b72cf271eef
h2entwicklung <entwicklung@h2-invent.com>
2023-11-09 10:37:42 +0100

    create migration and add SIP VIDEO ATTRIBUTE to the env and the is sip video to the user entity

---
+++ src/Service/ldap/LdapSipVideoGroupService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service\ldap;
+
+use App\dataType\LdapType;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Ldap\Entry;
+
+class LdapSipVideoGroupService
+{
+    private LdapType $ldapType;
+
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
+    public function getLdapType(): LdapType
+    {
+        return $this->ldapType;
+    }
+
+    public function setLdapType(LdapType $ldapType): void
+    {
+        $this->ldapType = $ldapType;
+    }
+
+    public function DetectUserIsSipVideoUser(User $user, Entry $ldapEntry)
+    {
+
+    }
+
+
+}
