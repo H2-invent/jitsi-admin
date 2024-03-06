a3da9177583d759b1f0baec6cc065520bb382629
h2entwicklung <entwicklung@h2-invent.com>
2024-03-06 11:57:22 +0100

    caaloutsession is initated if user has attributt to call when user is called from the addressbook

--- src/Service/Callout/CalloutService.php
+++ src/Service/Callout/CalloutService.php
@@ -7,6 +7,7 @@ use App\Entity\Rooms;
 use App\Entity\User;
 use App\Repository\LobbyWaitungUserRepository;
 use App\Service\adhocmeeting\AdhocMeetingService;
+use App\Service\adhocmeeting\AdhocMeetingWebsocketService;
 use App\Service\ThemeService;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
@@ -14,142 +15,149 @@ use Psr\Log\LoggerInterface;
 class CalloutService
 {
     public function __construct(
-        private EntityManagerInterface     $entityManager,
-        private AdhocMeetingService        $adhocMeetingService,
-        private ThemeService               $themeService,
-        private LoggerInterface            $logger,
+        private EntityManagerInterface       $entityManager,
+        private ThemeService                 $themeService,
+        private AdhocMeetingWebsocketService $adhocMeetingWebsocketService,
         private LobbyWaitungUserRepository $lobbyWaitungUserRepository,
     )
     {
     }
 
-    /**
-     * Starts the Callout Session Process.
-     * @param Rooms $rooms
-     * @param User $user
-     * @param User $inviter
-     * @return CalloutSession|null
-     */
-    public function initCalloutSession(Rooms $rooms, User $user, User $inviter): ?CalloutSession
-    {
-        return $this->createCallout($rooms, $user, $inviter);
-    }
+/**
+ * Starts the Callout Session Process.
+ * @param Rooms $rooms
+ * @param User $user
+ * @param User $inviter
+ * @return CalloutSession|null
+ */
+public
+function initCalloutSession(Rooms $rooms, User $user, User $inviter): ?CalloutSession
+{
+    dump('createCallout');
+    return $this->createCallout($rooms, $user, $inviter);
+}
 
-    /**
-     * Creates a new CalloutSession and rings the calles user, if this user online and propably not a phone user
-     * @param Rooms $rooms
-     * @param User $user
-     * @param User $inviter
-     * @return CalloutSession|null
-     */
-    public function createCallout(Rooms $rooms, User $user, User $inviter): ?CalloutSession
-    {
-        $callout = $this->checkCallout($rooms, $user);
-        if ($inviter === $user) {
-            return null;
-        }
-
-        if ($callout) {
-            if ($callout->getState() > 1) {//calloutsession is on hold
-                if ($callout->getLeftRetries() > 0) {
-                    $callout->setLeftRetries($callout->getLeftRetries() - 1);
-                    $callout->setState(CalloutSession::$INITIATED);
-                    $this->adhocMeetingService->sendAddhocMeetingWebsocket($user, $inviter, $rooms);
-                    $this->entityManager->persist($callout);
-                    $this->entityManager->flush();
-                }
-            }
-            return $callout;
-        }
-
-        $this->adhocMeetingService->sendAddhocMeetingWebsocket($user, $inviter, $rooms);
-
-        if (!$this->isAllowedToBeCalled($user)) {
-            return null;
-        }
-        if ($this->isalreadyInTheConfernce(user: $user)){
-            return null;
-        }
-
-        $callout = new CalloutSession();
-        $callout->setUser($user)
-            ->setRoom($rooms)
-            ->setCreatedAt(new \DateTime())
-            ->setInvitedFrom($inviter)
-            ->setUid(md5(uniqid()))
-            ->setState(CalloutSession::$INITIATED)
-            ->setLeftRetries($this->themeService->getApplicationProperties('CALLOUT_MAX_RETRIES'));
-        $this->entityManager->persist($callout);
-        $this->entityManager->flush();
-
-        return $callout;
-    }
-
-    /**
-     * checks is the callout session is already astablished
-     * @param Rooms $rooms
-     * @param User $user
-     * @return CalloutSession|null
-     */
-    public function checkCallout(Rooms $rooms, User $user): ?CalloutSession
-    {
-        return $this->entityManager->getRepository(CalloutSession::class)->findOneBy(['room' => $rooms, 'user' => $user]);
-    }
-
-    /**
-     * chechks either the user is alles to be called. is is done by check the env variable with the LDAP user properties
-     * and the corresponding spezial fields, which are loaded from the ldap
-     * @param User|null $user
-     * @return bool
-     */
-    public function isAllowedToBeCalled(?User $user): bool
-    {
-        return $this->getCallerIdForUser($user) !== null;
-    }
-
-    /**
-     * checks if the user is already invited or is already in the lobby
-     * @param User|null $user
-     * @return bool
-     */
-    public function isalreadyInTheConfernce(?User $user): bool
-    {
-        $lobbyUser = $this->lobbyWaitungUserRepository->findOneBy(array('user' => $user));
-        if ($lobbyUser) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Returns the CallerID which is mostly the telefonnumber from a user if this is configured
-     * @param User|null $user
-     * @return mixed|null
-     */
-    public function getCallerIdForUser(?User $user)
-    {
-        if (!$user) {
-            return null;
-        }
-        if (!$user->getLdapUserProperties()) {
-            return null;
-        }
-
-        $calloutFields = $this->themeService->getApplicationProperties('LDAP_CALLOUT_FIELDS');
-        foreach ($calloutFields as $ldapId => $fields) {
-            foreach ($fields as $field) {
-                if ($user->getLdapUserProperties()->getLdapNumber() === $ldapId) {
-                    if (isset($user->getSpezialProperties()[$field]) && $user->getSpezialProperties()[$field] !== '') {
-                        return $user->getSpezialProperties()[$field];
-                    }
-                }
-            }
-        }
+/**
+ * Creates a new CalloutSession and rings the calles user, if this user online and propably not a phone user
+ * @param Rooms $rooms
+ * @param User $user
+ * @param User $inviter
+ * @return CalloutSession|null
+ */
+public
+function createCallout(Rooms $rooms, User $user, User $inviter): ?CalloutSession
+{
+    $callout = $this->checkCallout($rooms, $user);
+    if ($inviter === $user) {
         return null;
     }
 
-    public function dialSuccessfull(User $user, Rooms $rooms): bool
-    {
+    if ($callout) {
+        if ($callout->getState() > 1) {//calloutsession is on hold
+            if ($callout->getLeftRetries() > 0) {
+                $callout->setLeftRetries($callout->getLeftRetries() - 1);
+                $callout->setState(CalloutSession::$INITIATED);
+                $this->adhocMeetingWebsocketService->sendAddhocMeetingWebsocket($user, $inviter, $rooms);
+                $this->entityManager->persist($callout);
+                $this->entityManager->flush();
+            }
+        }
+        return $callout;
+    }
+
+    $this->adhocMeetingWebsocketService->sendAddhocMeetingWebsocket($user, $inviter, $rooms);
+
+    if (!$this->isAllowedToBeCalled($user)) {
+        return null;
+    }
+    if ($this->isalreadyInTheConfernce(user: $user)) {
+        return null;
+    }
+
+    $callout = new CalloutSession();
+    $callout->setUser($user)
+        ->setRoom($rooms)
+        ->setCreatedAt(new \DateTime())
+        ->setInvitedFrom($inviter)
+        ->setUid(md5(uniqid()))
+        ->setState(CalloutSession::$INITIATED)
+        ->setLeftRetries($this->themeService->getApplicationProperties('CALLOUT_MAX_RETRIES'));
+    $this->entityManager->persist($callout);
+    $this->entityManager->flush();
+
+    return $callout;
+}
+
+/**
+ * checks is the callout session is already astablished
+ * @param Rooms $rooms
+ * @param User $user
+ * @return CalloutSession|null
+ */
+public
+function checkCallout(Rooms $rooms, User $user): ?CalloutSession
+{
+    return $this->entityManager->getRepository(CalloutSession::class)->findOneBy(['room' => $rooms, 'user' => $user]);
+}
+
+/**
+ * chechks either the user is alles to be called. is is done by check the env variable with the LDAP user properties
+ * and the corresponding spezial fields, which are loaded from the ldap
+ * @param User|null $user
+ * @return bool
+ */
+public
+function isAllowedToBeCalled(?User $user): bool
+{
+    return $this->getCallerIdForUser($user) !== null;
+}
+
+/**
+ * checks if the user is already invited or is already in the lobby
+ * @param User|null $user
+ * @return bool
+ */
+public
+function isalreadyInTheConfernce(?User $user): bool
+{
+    $lobbyUser = $this->lobbyWaitungUserRepository->findOneBy(array('user' => $user));
+    if ($lobbyUser) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Returns the CallerID which is mostly the telefonnumber from a user if this is configured
+ * @param User|null $user
+ * @return mixed|null
+ */
+public
+function getCallerIdForUser(?User $user)
+{
+    if (!$user) {
+        return null;
+    }
+    if (!$user->getLdapUserProperties()) {
+        return null;
+    }
+
+    $calloutFields = $this->themeService->getApplicationProperties('LDAP_CALLOUT_FIELDS');
+    foreach ($calloutFields as $ldapId => $fields) {
+        foreach ($fields as $field) {
+            if ($user->getLdapUserProperties()->getLdapNumber() === $ldapId) {
+                if (isset($user->getSpezialProperties()[$field]) && $user->getSpezialProperties()[$field] !== '') {
+                    return $user->getSpezialProperties()[$field];
+                }
+            }
+        }
+    }
+    return null;
+}
+
+public
+function dialSuccessfull(User $user, Rooms $rooms): bool
+{
 //        $calloutRepo = $this->entityManager->getRepository(CalloutSession::class);
 //        $calloutSession = $calloutRepo->findOneBy(array('room' => $rooms, 'user' => $user));
 //
@@ -166,5 +174,5 @@ class CalloutService
 //            $this->logger->debug('There is no calloutsession with this user and room');
 //        }
 //        return false;
-    }
+}
 }
