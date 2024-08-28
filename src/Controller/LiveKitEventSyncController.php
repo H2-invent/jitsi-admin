6742aa55c5f367ea46f0bdc74175def9d070454e
h2entwicklung <entwicklung@h2-invent.com>
2024-08-28 08:55:12 +0200

    unittests for live kit event sync

--- src/Controller/LiveKitEventSyncController.php
+++ src/Controller/LiveKitEventSyncController.php
@@ -7,6 +7,7 @@ use App\Service\webhook\RoomWebhookService;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,41 +15,75 @@ use Symfony\Component\Routing\Attribute\Route;
 class LiveKitEventSyncController extends AbstractController
 {
     private $token;
+
     public function __construct(
-        private RoomWebhookService $webhookService,
+        private RoomWebhookService    $webhookService,
         private ParameterBagInterface $parameterBag,
-        private LoggerInterface $logger
+        private LoggerInterface       $logger
     )
     {
-        $this->token = 'Bearer '. $this->parameterBag->get('LIVEKIT_EVENT_TOKEN');
+        $this->token = 'Bearer ' . $this->parameterBag->get('LIVEKIT_EVENT_TOKEN');
     }
 
     #[Route('/livekit/event', name: 'app_live_kit_event_sync')]
     public function index(Request $request): Response
     {
-        $check = CheckAuthorizationService::checkHEader($request,$this->token);
-        if ($check){
+        $check = CheckAuthorizationService::checkHEader($request, $this->token);
+        if ($check) {
             return $check;
         }
-        $data = json_decode($request->get('data'),true);
+        $data = json_decode($request->getContent(), true);
         $eventType = $data['event'];
-        switch ($eventType){
+        $res = ['error' => false];
+        switch ($eventType) {
             case 'room_finished':
-
+                $res = $this->webhookService->roomDestroyed(false,
+                    null,
+                    $data['room']['sid'],
+                    $data['createdAt']
+                );
                 break;
             case 'room_started':
+                $res = $this->webhookService->roomCreated(
+                    $data['room']['name'],
+                    false,
+                    null,
+                    $data['room']['sid'],
+                    $data['room']['creationTime']
+                );
                 break;
             case 'participant_left':
+                $res = $this->webhookService->roomParticipantLeft(
+                    false,
+                    null,
+                    $data['participant']['sid'],
+                    $data['createdAt'],
+                    null
+                );
                 break;
             case 'participant_joined':
+                $res =  $this->webhookService->roomParticipantJoin(false, null,
+                    $data['room']['sid'],
+                    $data['participant']['sid'],
+                    $data['participant']['joinedAt'],
+                    $data['participant']['name']
+                );
                 break;
             default:
-                $this->logger->error('unregistered Event found',['event'=>$eventType]);
+                $this->logger->error('unregistered Event found', ['event' => $eventType]);
                 break;
         }
-        return $this->render('live_kit_event_sync/index.html.twig', [
-            'controller_name' => 'LiveKitEventSyncController',
-        ]);
+        if (!$res){
+            $res = ['error' => false];
+        }else{
+            $res=[
+                'error'=>$res
+            ];
+        }
+        return new JsonResponse($res);
+
     }
+
 }
+
 ;
