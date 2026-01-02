2bdc91957b3fd24ae41a8cbfd12c2a431b4bf9d6
Andreas <58698758+holzi1005@users.noreply.github.com>
2026-01-02 10:50:52 +0100

    Update config.mjs

--- nodejs/config.mjs
+++ nodejs/config.mjs
@@ -5,3 +5,7 @@ export var AWAY_TIME =process.env.AWAY_TIME || 5;
 export var DEFAULT_STATE =process.env.DEFAULT_STATE || 'offline';
 export var KEY_FILE =process.env.KEY_FILE || './tls_certificate/key.pem';
 export var CERT_FILE =process.env.CERT_FILE || './tls_certificate/cert.pem';
+
+export const REDIS_SENTINELS = process.env.REDIS_SENTINELS || "127.0.0.1:26379";
+export const REDIS_MASTER_NAME = process.env.REDIS_MASTER_NAME || "mymaster";
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD || null;
