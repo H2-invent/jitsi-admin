20803077ae8821c34c730c28961cfca1c721e096
reimar <me@reimar.org>
2026-02-24 14:19:03 +0100

    Add pure CSS imports to black.js, join.js, and joinBlack.js for consistent styling and improved build performance

--- assets/js/black.js
+++ assets/js/black.js
@@ -2,4 +2,13 @@
  * Welcome to your app's main JavaScript file!
  *
  */
+// Pure CSS imports (bypass sass compiler) - must match app.js imports
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'mdb-ui-kit/css/mdb.min.css';
+import 'flatpickr/dist/flatpickr.min.css';
+import 'jquery-confirm/css/jquery-confirm.css';
+import 'h2-invent-apps/css/h2-invent-apps.css';
+import 'css-star-rating/css/star-rating.css';
+import '@holema/h2datetimepicker/css/dateTimePicker.css';
+
 import '../css/black.scss';
