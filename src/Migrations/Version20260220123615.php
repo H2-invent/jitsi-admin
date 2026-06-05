8d2f47434c7af4997cf5b185559b1bc447fb0ab8
Andreas <58698758+holzi1005@users.noreply.github.com>
2026-06-05 14:19:01 +0200

    Update Version20260220123615.php

--- src/Migrations/Version20260220123615.php
+++ src/Migrations/Version20260220123615.php
@@ -1,15 +1,9 @@
 <?php
-
 declare(strict_types=1);
-
 namespace DoctrineMigrations;
-
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20260220123615 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,11 +13,15 @@ final class Version20260220123615 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE server ADD is_provisioning_enabled TINYINT(1) DEFAULT NULL');
+        $table = $schema->getTable('server');
+        $table->addColumn('is_provisioning_enabled', 'boolean', [
+            'default' => null,
+            'notnull' => false,
+        ]);
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE server DROP is_provisioning_enabled');
+        $schema->getTable('server')->dropColumn('is_provisioning_enabled');
     }
 }
