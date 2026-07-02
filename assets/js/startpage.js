3ebfe83951d08243b07085a7c3aadace22e055ff
rgasch <robert.gasch@h2-invent.com>
2026-07-02 13:19:45 +0200

    Added language selector + minor style changes (WIP)

--- assets/js/startpage.js
+++ assets/js/startpage.js
@@ -8,3 +8,12 @@ import('bootstrap');
 $('#navToggle').on('click', function () {
   $('.hp-nav-right').toggleClass('hp-nav-open');
 });
+
+/* Language Dropdown */
+$('#langToggle').on('click', function (e) {
+  e.stopPropagation();
+  $('#langMenu').toggleClass('hp-lang-open');
+});
+$(document).on('click', function () {
+  $('#langMenu').removeClass('hp-lang-open');
+});
