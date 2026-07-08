1d2c033d2c89adfce89924b774845a6f6472def8
h2entwicklung <entwicklung@h2-invent.com>
2026-07-08 22:11:42 +0200

    * add new header image

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/theme', name: 'app_theme_')]
+#[Route('/room/theme', name: 'app_theme_')]
 class ThemeController extends AbstractController
 {
     public function __construct(
