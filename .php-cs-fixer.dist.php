f89b6a3dade75a110d695c0b54255955b346ed3f
Martina Scholz <martina.scholz@h2-invent.com>
2026-07-09 14:54:12 +0200

    Add friendsofphp/php-cs-fixer to symfony.lock with version 3.95

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->notPath([
+        'config/bundles.php',
+        'config/reference.php',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        // Basic ruleset PSR 12
+        '@PSR12' => true,
+    ])
+    ->setFinder($finder)
+;
