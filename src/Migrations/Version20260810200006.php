8ac29ebf410fb8aece3fc9b57c2ff0dff7185e1c
rgasch <robert.gasch@h2-invent.com>
2026-08-12 19:39:08 +0200

    Fixed broken pipeline migration

--- src/Migrations/Version20260810200006.php
+++ src/Migrations/Version20260810200006.php
@@ -19,13 +19,24 @@ final class Version20260810200006 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $table = $schema->getTable(self::TABLE_NAME);
-        $table->addIndex(['keycloak_group'], self::INDEX_NAME);
+        $this->addSql(
+            sprintf(
+                'CREATE INDEX %s ON %s (%s(128))',
+                self::INDEX_NAME,
+                self::TABLE_NAME,
+                'keycloak_group'
+            )
+        );
     }
 
     public function down(Schema $schema): void
     {
-        $table = $schema->getTable(self::TABLE_NAME);
-        $table->dropIndex(self::INDEX_NAME);
+        $this->addSql(
+            sprintf(
+                'DROP INDEX %s ON %s',
+                self::INDEX_NAME,
+                self::TABLE_NAME
+            )
+        );
     }
 }
