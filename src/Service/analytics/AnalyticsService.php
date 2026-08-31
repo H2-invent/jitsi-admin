4e3b9041055add8dac5d936a6030911ace00a045
rgasch <robert.gasch@h2-invent.com>
2026-08-31 10:55:06 +0200

    Don't fetch all rooms/users/etc. but rather use COUNT() functions

--- src/Service/analytics/AnalyticsService.php
+++ src/Service/analytics/AnalyticsService.php
@@ -2,9 +2,6 @@
 
 namespace App\Service\analytics;
 
-use App\Entity\Rooms;
-use App\Entity\Server;
-use App\Entity\User;
 use App\Service\Theme\ThemeService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
@@ -26,41 +23,26 @@ class AnalyticsService
 
     public function gatherInformations(): array
     {
-        $average = 0;
-        $averageCounter = 0;
+        $em = $this->entityManager;
         $res = ['data' => 'jitsi-admin'];
-        $rooms = $this->entityManager->getRepository(Rooms::class)->findAll();
-        $res['rooms'] = count($rooms);
-        $users = $this->entityManager->getRepository(User::class)->findAll();
-        $res['users'] = count($users);
-        $usersKC = $this->entityManager->getRepository(User::class)->findUsersWithKC();
-        $res['kcUser'] = count($usersKC);
+        $res['rooms'] = (int)$em->createQuery('SELECT COUNT(r) FROM App\Entity\Rooms r')->getSingleScalarResult();
+        $res['users'] = (int)$em->createQuery('SELECT COUNT(u) FROM App\Entity\User u')->getSingleScalarResult();
+        $res['kcUser'] = (int)$em->createQuery('SELECT COUNT(u) FROM App\Entity\User u WHERE u.keycloakId IS NOT NULL')->getSingleScalarResult();
+        $res['openRooms'] = (int)$em->createQuery('SELECT COUNT(r) FROM App\Entity\Rooms r WHERE r.totalOpenRooms = true')->getSingleScalarResult();
         $res['jitsiadmin_version'] = $this->parameterBag->get('laF_version');
-        $openRooms = $this->entityManager->getRepository(Rooms::class)->findBy(['totalOpenRooms' => true]);
-        $res['openRooms'] = count($openRooms);
-        $url = [];
-        foreach ($rooms as $data) {
-            if (!in_array($data->getHostUrl(), $url)) {
-                $url[] = $data->getHostUrl();
-            }
-            if (count($data->getUser()) > 0) {
-                $average += count($data->getUser());
-                $averageCounter++;
-            }
 
-        }
-        $average = $averageCounter!=0?($average / $averageCounter):0;
-        $res['average_room_size'] = $average;
-        $res['urls'] = $url;
-        $server = $this->entityManager->getRepository(Server::class)->findAll();
-        $res['servers_amount'] = count($server);
-        $serverArr = [];
-        foreach ($server as $data2) {
-            $serverArr[] = $data2->getUrl();
-        }
-        $res['server_url'] = $serverArr;
+        $urls = $em->createQuery('SELECT DISTINCT r.hostUrl FROM App\Entity\Rooms r WHERE r.hostUrl IS NOT NULL')->getSingleColumnResult();
+        $res['urls'] = array_values(array_filter($urls));
+        $average = $em->getConnection()->fetchOne(
+            'SELECT AVG(cnt) FROM (SELECT COUNT(*) AS cnt FROM rooms_user GROUP BY rooms_id HAVING COUNT(*) > 0) t'
+        );
+
+        $res['average_room_size'] = $average !== false ? (float)$average : 0;
+        $res['servers_amount'] = (int)$em->createQuery('SELECT COUNT(s) FROM App\Entity\Server s')->getSingleScalarResult();
+        $serverUrl = $em->createQuery('SELECT s.url FROM App\Entity\Server s')->getSingleColumnResult();
+        $res['server_url'] = array_values(array_filter($serverUrl));
         $theme = $this->themeService->showAllThemes();
-        if ($theme){
+        if ($theme) {
             $res['theme'] = $theme;
         }
 
